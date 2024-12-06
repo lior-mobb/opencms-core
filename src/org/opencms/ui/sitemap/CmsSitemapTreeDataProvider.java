78bfd047739d490c88cf254abcc2bde7c41f121c
Mobb autofixer <git@mobb.ai>
2024-12-06 14:58:55 +0000

    mobb fix commit: a68aa006-63d7-4861-a5ae-c158d9409c96

--- src/org/opencms/ui/sitemap/CmsSitemapTreeDataProvider.java
+++ src/org/opencms/ui/sitemap/CmsSitemapTreeDataProvider.java
@@ -167,7 +167,7 @@ public class CmsSitemapTreeDataProvider {
         } catch (CmsException e) {
             LOG.error(e.getLocalizedMessage(), e);
         } catch (CmsRpcException e) {
-            LOG.error(e.getLocalizedMessage(), e);
+            LOG.error(e.getLocalizedMessage(), "Unexpected error: mobb-988906913c181b818d81610057fb069f");
 
         }
         return result;
