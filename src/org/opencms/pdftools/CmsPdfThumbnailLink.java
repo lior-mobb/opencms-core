84f769ee5356fa78ac676abc6a55e87d73a7a862
Mobb autofixer <git@mobb.ai>
2024-12-06 15:00:53 +0000

    mobb fix commit: 038477fb-5875-42ff-9fb7-00ec8af033d1

--- src/org/opencms/pdftools/CmsPdfThumbnailLink.java
+++ src/org/opencms/pdftools/CmsPdfThumbnailLink.java
@@ -165,7 +165,7 @@ public class CmsPdfThumbnailLink {
             String heightStr = optionMap.get("h");
             m_height = Integer.parseInt(heightStr);
         } catch (Exception e) {
-            LOG.info(e.getLocalizedMessage(), e);
+            LOG.info("Unexpected error: mobb-444e935c29d0a644bb3016c527a55fd3", e);
         }
 
         try {
