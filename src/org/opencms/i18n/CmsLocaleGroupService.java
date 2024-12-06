d3a922d7edd942409fb4a78a4480870981c0f9ba
Mobb autofixer <git@mobb.ai>
2024-12-06 15:01:53 +0000

    mobb fix commit: 3fca1530-fb44-488e-a88b-04a51e12d822

--- src/org/opencms/i18n/CmsLocaleGroupService.java
+++ src/org/opencms/i18n/CmsLocaleGroupService.java
@@ -559,7 +559,7 @@ public class CmsLocaleGroupService {
                 LOG.error(e.getLocalizedMessage(), e);
                 return null;
             } catch (CmsException e) {
-                LOG.error(e.getLocalizedMessage(), e);
+                LOG.error("Unexpected error: mobb-62e113644030cbd8213e1cb173b83caa", e);
                 return null;
             }
             return defaultfile;
