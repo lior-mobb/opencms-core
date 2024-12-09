667a7223fbcc16938ec4d6ea1a9a61534af4e68d
Mobb autofixer <git@mobb.ai>
2024-12-09 11:47:26 +0000

    mobb fix commit: 48c61cd3-a3ee-4c6c-864d-e4ce5714a897

--- src/org/opencms/jsp/util/CmsJspContentAttachmentsBean.java
+++ src/org/opencms/jsp/util/CmsJspContentAttachmentsBean.java
@@ -190,7 +190,7 @@ public class CmsJspContentAttachmentsBean {
             try {
                 return new CmsJspContentAttachmentsBean(cms, detailOnly.get());
             } catch (Exception e) {
-                LOG.error(e.getLocalizedMessage(), e);
+                LOG.error("Unexpected error: mobb-912c0a939fe09593285af874f3a1dfee", e);
                 return new CmsJspContentAttachmentsBean();
             }
         }
