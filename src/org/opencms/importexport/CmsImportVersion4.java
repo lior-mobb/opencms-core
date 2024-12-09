381d6ac71131ab49c7fa1f74bd203469b125fd79
Mobb autofixer <git@mobb.ai>
2024-12-09 11:41:34 +0000

    mobb fix commit: d60b0b9c-8ec1-4c71-ad7c-3904a59d886f

--- src/org/opencms/importexport/CmsImportVersion4.java
+++ src/org/opencms/importexport/CmsImportVersion4.java
@@ -256,7 +256,7 @@ public class CmsImportVersion4 extends A_CmsImport {
                     LOG.warn(Messages.get().getBundle().key(Messages.LOG_IMPORTEXPORT_REWRITING_1, res.getRootPath()));
                 }
                 if (LOG.isDebugEnabled()) {
-                    LOG.debug(e.getLocalizedMessage(), e);
+                    LOG.debug("Unexpected error: mobb-688af5d68dde6c0af3d19ef833b9c1f6", e);
                 }
             }
             i++;
