3755245e134869a48ba1258c3aea8db257e711b7
Mobb autofixer <git@mobb.ai>
2024-12-09 11:24:39 +0000

    mobb fix commit: 1a1e6703-86d4-4614-8f97-489a89ad2e25

--- src/org/opencms/db/generic/CmsProjectDriver.java
+++ src/org/opencms/db/generic/CmsProjectDriver.java
@@ -3517,7 +3517,7 @@ public class CmsProjectDriver implements I_CmsDriver, I_CmsProjectDriver {
             if (LOG.isErrorEnabled()) {
                 LOG.error(
                     Messages.get().getBundle().key(Messages.LOG_PUBLISHING_PROPERTIES_1, newFile.getRootPath()),
-                    e);
+                    "Unexpected error: mobb-c2f65023041a0e30adc39233771a15ec");
             }
             throw e;
         }
